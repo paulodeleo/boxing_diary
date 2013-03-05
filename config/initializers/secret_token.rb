b94d1e838bd47b9262090f82549718f59dea27b0
Paulo Henrique Deléo <paulodeleo@gmail.com>
2013-03-04 23:24:02 -0300

    Initial commit. Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoxingDiary::Application.config.secret_token = '591f75d57f2cdc74800c7d82c452c6b383338a53e16d337ef9d20ecf7705885266c31a987d2c00d1e255cce836338e1fafbd48f3731c21f68dd554c0ea31a988'
